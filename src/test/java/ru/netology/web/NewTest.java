197f1bab4c826cc4de8ac6e1703073336f55f4aa
NadezhdaPogodina <pogodinadia@inbox.ru>
2023-05-25 23:43:57 +0300

    Add to test

--- src/test/java/ru/netology/web/NewTest.java
+++ src/test/java/ru/netology/web/NewTest.java
@@ -4,9 +4,13 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
 
 public class NewTest {
     private WebDriver driver;
@@ -35,8 +39,14 @@ public class NewTest {
     }
 
     @Test
-    void shouldTestUI() {
+    void shouldTestVI() throws InterruptedException {
         driver.get("http://localhost:9999/");
+        Thread.sleep(8000);
+        List<WebElement> inputs = driver.findElements(By.tagName("input"));
+        inputs.get(0).sendKeys("123");
+        inputs.get(1).sendKeys("+7999888888");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
     }
 
 }
